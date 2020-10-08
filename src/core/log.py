f3013ee0a225a2c973f6068e57c1703a07e7c31b
nekoffski <mrnek135@gmail.com>
2020-10-08 15:05:30 +0200

    Foundations

---
+++ src/core/log.py
@@ -0,0 +1,15 @@
+import logging
+
+
+def get_logger(name: str) -> logging.Logger:
+    log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    root = logging.getLogger(name)
+
+    formatter = logging.Formatter(log_format)
+    root.setLevel(logging.DEBUG)
+
+    console = logging.StreamHandler()
+    console.setFormatter(formatter)
+
+    root.addHandler(console)
+    return root
---
+++ src/run.py
@@ -0,0 +1,34 @@
+import signal
+import os
+import uvloop
+import asyncio
+import aiomisc
+from core.log import get_logger
+
+
+log = get_logger("main")
+
+
+@aiomisc.receiver(aiomisc.entrypoint.PRE_START)
+async def pre_init(entrypoint, services) -> None:
+    log = get_logger("main.init")
+
+    log.info("setting up signal handler")
+
+    async def shutdown() -> None:
+        log.info("received SIGINT/SIGTERM shutting down all active task")
+        tasks = [t for t in asyncio.Task.all_tasks(
+        ) if t is not asyncio.Task.current_task()]
+        for task in tasks:
+            task.cancel()
+        await asyncio.gather(*tasks, return_exceptions=True)
+        log.info("stopping event loop")
+        asyncio.get_event_loop().stop()
+
+    for sig in [signal.SIGTERM, signal.SIGINT]:
+        asyncio.get_event_loop().add_signal_handler(
+            sig, lambda: asyncio.create_task(shutdown())
+        )
+
+
+print("hello world!")
