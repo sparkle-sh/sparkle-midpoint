6ede4b50327973005c9762e08b62c315b401042a
Kacper Czyż <mrnek135@gmail.com>
2020-10-09 22:41:44 +0200

    Implement config class

--- src/api/service.py
+++ src/api/service.py
@@ -1,24 +1,23 @@
 import aiomisc
 import asyncio
 import sanic
+import typing
 from core.log import get_logger
 from api.endpoint.endpoint import setup_endpoints 
 
 log = get_logger("api.service")
-PORT = 7776
-HOST = '0.0.0.0'
+
 
 class ApiService(aiomisc.Service):
-    def __init__(self):
-        self.port = PORT
-        self.host = HOST
-        self.app = sanic.Sanic(name= "sparkle-midpoint")
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.app = sanic.Sanic(name= "sparkle-api-gateway")
     
     async def start(self):
         log.info("Starting api service")
         await setup_endpoints(self.app)
         await asyncio.create_task(
-            self.app.create_server(host = self.host, port = self.port, return_asyncio_server = True))
+            self.app.create_server(host=self.cfg.api.host, port=self.cfg.api.port, return_asyncio_server = True))
     
     async def stop(self, exception: Exception = None):
         log.info("Stopping api service")
