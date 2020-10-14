1ccc42bf513407123f4d7916ad2989f453344b95
Kacper Czyż <mrnek135@gmail.com>
2020-10-14 17:25:12 +0200

    Add test coverage tool

--- bin/deps.sh
+++ bin/deps.sh
@@ -3,6 +3,9 @@
 sudo apt-get install -y python3.7
 sudo apt-get install -y python3-setuptools
 sudo apt-get install -y python3.7-venv
+sudo apt-get install -y python3-dev
+sudo apt-get install gcc
+
 
 sudo python3.7 -m ensurepip
 sudo python3.7 -m pip install virtualenv
--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source ./venv/bin/activate
-UTILS_PATH=$(pwd)/test/unit/test_utils PYTHONPATH=./src ./venv/bin/py.test `find ./test/unit -iname *tests.py | xargs` --verbose --disable-pytest-warnings
+UTILS_PATH=$(pwd)/test/unit/test_utils PYTHONPATH=./src ./venv/bin/coverage run -m pytest `find ./test/unit -iname *tests.py | xargs` --verbose --disable-pytest-warnings
+
