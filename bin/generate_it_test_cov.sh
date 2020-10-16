df20e5cf9269be0433e4a636b6238fb166fc553e
nekoffski <mrnek135@gmail.com>
2020-10-16 13:52:37 +0200

    cov

--- bin/generate_it_test_cov.sh
+++ bin/generate_it_test_cov.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 if [ ! -d ./sparkle-midpoint-cov ]; then
-	echo "Test coverage files not generated, skipping"
+	echo "Integartion tests coverage files not generated, skipping"
 	exit 0
 fi
 
+mkdir it-cov
+
+for f in `ls ./sparkle-midpoint-cov`; do
+	mkdir tmp
+	tar -C ./tmp -zxvf ./sparkle-midpoint-cov/$f
+	cp ./tmp/.coverage ./it-cov/.coverage_$f
+	rm tmp
+done
+
+cd it-cov
+coverage report -m
+coverage html
 
