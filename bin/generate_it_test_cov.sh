7be103c1ad7f60741276c993c45bcbb592ae0fa5
nekoffski <mrnek135@gmail.com>
2020-10-16 13:58:20 +0200

    cov

--- bin/generate_it_test_cov.sh
+++ bin/generate_it_test_cov.sh
@@ -7,11 +7,13 @@ fi
 
 mkdir it-cov
 
+ls ./sparkle-midpoint-cov
+
 for f in `ls ./sparkle-midpoint-cov`; do
 	mkdir tmp
 	tar -C ./tmp -zxvf ./sparkle-midpoint-cov/$f
 	cp ./tmp/.coverage ./it-cov/.coverage_$f
-	rm tmp
+	rm tmp -rf
 done
 
 cd it-cov
