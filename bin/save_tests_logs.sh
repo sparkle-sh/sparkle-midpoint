90af0dbfde4de31c4a0882a339b233b2486e5909
nekoffski <mrnek135@gmail.com>
2020-10-20 18:44:55 +0200

    fFush

--- bin/save_tests_logs.sh
+++ bin/save_tests_logs.sh
@@ -5,11 +5,11 @@ mkdir tmp
 
 for l in `ls *.tar`; do
 	tar -C ./tmp -xvf ./$l
-	mv ./tmp/sparkle-midpoint.log ./`echo $f | cut -d '.' -f1-3`.log
+	mv ./tmp/sparkle-midpoint.log ./`echo $l | cut -d '.' -f1-3`.log
 done
 
 rm *.tar
-rm tmp -f
+rm -rf tmp
 
 cd ..
 ls ./logs
