f10785822f5b102909c726ff11a09f5129236a07
nekoffski <mrnek135@gmail.com>
2020-10-31 12:21:05 +0100

    fix

--- src/scheduler/task/actions.py
+++ src/scheduler/task/actions.py
@@ -1,2 +1,14 @@
+from src.transmission.connector_client import ConnectorClient
 
-class Action
+
+class Action(object):
+    def execute(self, connector_client: ConnectorClient):
+        raise NotImplementedError
+
+
+class SwitchDeviceStateAction(Action):
+    def __init__(self, state_changer):
+        self.state_changer = state_changer
+
+    def execute(self, connector_client: ConnectorClient):
+        pass
