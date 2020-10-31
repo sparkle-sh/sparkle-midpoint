c46adebe3d51ae4a75f16a541794aa3efcbc8033
nekoffski <mrnek135@gmail.com>
2020-10-31 12:35:06 +0100

    fix

--- test/unit/scheduler/task_tests.py
+++ test/unit/scheduler/task_tests.py
@@ -4,7 +4,7 @@ from src.scheduler.task.task import *
 
 class TaskTests(asynctest.TestCase):
     async def test_when_update_on_base_task_expect_exception(self):
-        t = Task(None, None)
+        t = Task(None, None, None)
         try:
             await t.update()
         except NotImplementedError:
