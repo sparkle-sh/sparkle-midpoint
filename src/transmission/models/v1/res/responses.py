227feb6fcba2731ad97e9b603110df6e7d3c4693
aniawala <anna.walaszek97@gmail.com>
2020-10-31 11:25:55 +0100

    Fix

--- src/transmission/models/v1/res/responses.py
+++ src/transmission/models/v1/res/responses.py
@@ -2,7 +2,7 @@ from .handshake_response import HandshakeResponse
 from .ack_response import AckResponse
 from .get_device_datasheet_response import GetDeviceDatasheetResponse
 from .get_device_state_response import GetDeviceStateResponse
-from .get_sensor_value_response import GetSensorValueResponse
+from .get_device_value_response import GetDeviceValueResponse
 from .list_devices_response import ListDevicesResponse
 
 
@@ -11,6 +11,6 @@ HEADER_TO_RESPONSE = {
     "ack_response": AckResponse,
     "get_device_state_response": GetDeviceStateResponse,
     "get_device_datasheet_response": GetDeviceDatasheetResponse,
-    "get_sensor_value_response": GetSensorValueResponse,
+    "get_device_value_response": GetDeviceValueResponse,
     "list_devices_response": ListDevicesResponse
 }
